8bbce61fa7213a58f479c07308764f158b7b44d6
andresdanielmtz <autummata@gmail.com>
2026-02-27 00:30:15 -0600

    remove: gitignore

--- src/main/java/com/example/restservice/model/CloudProvider.java
+++ src/main/java/com/example/restservice/model/CloudProvider.java
@@ -1,4 +1,7 @@
 package com.example.restservice.model;
 
+/**
+ * Abstract interface representing a cloud provider. This can be implemented by specific cloud providers like AWS, Azure, GCP, etc.
+ */
 public interface CloudProvider {
 }
